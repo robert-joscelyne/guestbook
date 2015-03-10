296ee279c54d4008d38bb845e039b84d8a80b31e
Robert Joscelyne <robert.joscelyne@gmail.com>
2015-03-10 18:58:57 +0000

    Inital version of the Guestbook project

---
+++ src/guestbook/core.clj
@@ -0,0 +1,8 @@
+(ns guestbook.core
+  (:require [guestbook.handler :refer [app]]
+    [ring.adapter.jetty :refer [run-jetty]])
+  (:gen-class))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    (run-jetty app {:port port :join? false})))
